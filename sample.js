0ebfd5dc0831d6d3806933f3fe95f12ece8c390d
rajarajeswaric <22ece.rajarajeswari.c@skct.edu.in>
2024-07-24 10:40:35 +0530

    commit

--- sample.js
+++ sample.js
@@ -11,3 +11,15 @@ let fruits = ["Apple","Banana","Cherry"]
 console.log(fruits[0])
 console.log(fruits[1])
 console.log(fruits[2])
+
+function greet()
+{
+    console.log("Be Happy")
+}
+greet()
+
+let now = new Date;
+console.log(now)
+
+let sum = 30 + 10;
+console.log(sum)
