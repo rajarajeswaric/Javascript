8a17f6f95e7de3d63af902fb5b906c6779ef8de3
rajarajeswaric <22ece.rajarajeswari.c@skct.edu.in>
2024-07-24 10:22:57 +0530

    commit

--- sample.js
+++ sample.js
@@ -1,14 +1,14 @@
 function exampleLet()
 {
-    let x=10;
-    console.log(x)
+    const y=10;
+    console.log(y)
 
  if(true)
  {
-    let x =20;
-    console.log(x)
+    let y=20;
+    console.log(y)
  }
- console.log(x)
+ console.log(y)
 }
 exampleLet()
     
