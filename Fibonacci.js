0cc0c09314dc6f8b3856906397826cb127bd4380
rajarajeswaric <22ece.rajarajeswari.c@skct.edu.in>
2024-07-26 10:24:09 +0530

    commit

--- Fibonacci.js
+++ Fibonacci.js
@@ -1,1 +1,14 @@
+function fibo()
 
+  let n1=0
+  let n2 = 1
+  let x=5
+  console.log(n1,n2);
+  for(let i=2;i<=x;i++)
+  {
+    let temp = n2;
+    n2 = n1 + n2;
+    console.log(n2)
+    n1=temp
+  }
+console.log(fibo())
